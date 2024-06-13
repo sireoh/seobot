7d6d49b28f15d9a8fee412f666380573946fa2ca
Vincent <eohndk@gmail.com>
2024-06-13 02:23:32 -0700

    added titlegen, and first

--- bot.js
+++ bot.js
@@ -1,3 +1,8 @@
+/* #region local database */
+var firstData = ["noone atm"];
+var titlegenData = [];
+/* #endregion local database */
+
 /* #region importing packages */
 const express = require('express');
 const session = require('express-session');
@@ -5,6 +10,13 @@ const tmi = require('tmi.js');
 const axios = require('axios');
 const app = express();
 require('dotenv').config();
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+const {
+	printWords,
+	printEmojis
+} = require("./scripts/titlegen.js");
 
 const PORT = 3055;
 const PARAMS = new URLSearchParams({
@@ -52,6 +64,7 @@ app.get("/token", async (req, res) => {
 			}
 		});
 		const access_token = tokenResponse.data.access_token;
+		req.session.code = code;
 		req.session.access_token = access_token;
 		res.redirect("/auth");
 	} catch (error) {
@@ -63,9 +76,19 @@ app.get("/token", async (req, res) => {
 app.get("/auth", (req, res) => {
 	res.send({
 		data: {
+		authcode: req.session.code,
 		access_token: req.session.access_token,
 		}
 	});
+});
+
+app.get("/first", (req, res) => {
+	firstData[0] = req.query.user;
+	res.render("first", {data: firstData[0]});
+});
+
+app.get("/titlegen", (req, res) => {
+	res.render("titlegen", {data: titlegenData[0]});
 });
   
 app.listen(PORT, () => {
@@ -73,10 +96,128 @@ app.listen(PORT, () => {
 });
 /* #endregion routing */
 
+/* #region commands */
 client.on('message', (channel, tags, message, self) => {
+	const command = message.toLowerCase();
 	if(self) return;
 
-	if(message.toLowerCase() === 'marco') {
-		client.say(channel, "polo");
+	if(command === 'test') {
+		client.say(channel, "test");
+	}
+
+	if(command.startsWith('!titlegen')) {
+		titlegenCmd(channel, command);
 	}
 });
+/* #endregion commands */
+
+/* #region command logic */
+function firstCmd(channel, message) {
+	firstData[0] = message.split(" ")[1];
+}
+
+function titlegenCmd(channel, message) {
+	//Custom Title
+	let titlestr = "";
+	let arr = message.split(" ");
+	if (arr[2]) {
+		for (let i = 2; i < arr.length; i++) {
+			titlestr += arr[i];
+			if (i < arr.length - 1) {
+				titlestr += " ";
+			}
+		}
+		titlegenData[1] = titlestr;
+	}
+	console.log(titlestr);
+
+	/*
+	empty: [mic on], and random.
+	off: [mic off], and random.
+	Option 2: custom.
+	*/
+	switch(arr[1]) {
+		case "off":
+			titlegenData[1] = printWords();
+			titlegenData[2] = printEmojis();
+			titlegenData[0] = "[mic off] " + titlegenData[1] + " "
+				+ titlegenData[2];
+			client.say(channel, "generated title🖨, with mic-off 🎤❌");
+			console.log({
+				mode: "off",
+				custom: "false",
+				data: titlegenData[0]
+			});
+			break;
+		
+		case "toggleoff":
+			titlegenData[0] = "[mic off] " + titlegenData[1] + " "
+				+ titlegenData[2];
+				client.say(channel, `title shows mic "off" 🎤❌ now in overlay🖨`);
+			console.log({
+				mode: "off",
+				custom: "false",
+				data: titlegenData[0]
+			});
+			break;
+
+		case "toggleon":
+			titlegenData[0] = "[mic on] " + titlegenData[1] + " "
+				+ titlegenData[2];
+				client.say(channel, `title shows mic "on" 🎤✅ now in overlay🖨`);
+			console.log({
+				mode: "off",
+				custom: "false",
+				data: titlegenData[0]
+			});
+			break;
+		
+		case "customon":
+			titlegenData[2] = printEmojis();
+			titlegenData[0] = "[mic on] " + titlegenData[1] + " "
+				+ titlegenData[2];
+			client.say(channel, "generated custom title🖨, with mic-on 🎤✅");
+			console.log({
+				mode: "off",
+				custom: "true",
+				data: titlegenData[0]
+			});
+			break;
+
+		case "customoff":
+			titlegenData[2] = printEmojis();
+			titlegenData[0] = "[mic off] " + titlegenData[1] + " "
+				+ titlegenData[2];
+			client.say(channel, "generated custom title🖨, with mic-off 🎤❌");
+			console.log({
+				mode: "on",
+				custom: "true",
+				data: titlegenData[0]
+			});
+			break;
+
+		case "what":
+			client.say(channel, "/me the title is ...");
+			client.say(channel, titlegenData[1] + " " + titlegenData[2]);
+			break;
+		
+		case "set":
+			client.say(channel, "!settitle " + titlegenData[1] + " "
+				+ titlegenData[2]);
+			break;
+
+		default:
+			titlegenData[1] = printWords();
+			titlegenData[2] = printEmojis();
+			titlegenData[0] = "[mic on] " + titlegenData[1] + " "
+				+ titlegenData[2];
+			client.say(channel, "generated title🖨, with mic-on 🎤✅");
+			console.log({
+				mode: "on",
+				custom: "false",
+				data: titlegenData[0]
+			});
+			break;
+	};
+}
+/* #endregion command logic */
